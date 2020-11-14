10b38c3d7d0edd41c6fbf7226146b35cce943236
ab13696802@gmail.com <lcs3623@naver.com>
2020-11-14 22:06:04 +0900

    frontend(signUpv2)

--- Front Example/js/main.js
+++ Front Example/js/main.js
@@ -1,6 +1,3 @@
-
-var id = document.querySelector('#id');
-
 var pw1 = document.querySelector('#pswd1');
 var pwMsg = document.querySelector('#alertTxt');
 var pwImg1 = document.querySelector('#pswd1_img1');
@@ -28,7 +25,6 @@ var error = document.querySelectorAll('.error_next_box');
 /*이벤트 핸들러 연결*/
 
 
-id.addEventListener("focusout", checkId);
 pw1.addEventListener("focusout", checkPw);
 pw2.addEventListener("focusout", comparePw);
 userName.addEventListener("focusout", checkName);
@@ -52,19 +48,21 @@ mobile.addEventListener("focusout", checkPhoneNum);
 /*콜백 함수*/
 
 
-function checkId() {
-    var idPattern = /[a-zA-Z0-9_-]{5,20}/;
-    if(id.value === "") {
-        error[0].innerHTML = "필수 정보입니다.";
-        error[0].style.display = "block";
-    } else if(!idPattern.test(id.value)) {
-        error[0].innerHTML = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.";
-        error[0].style.display = "block";
+function isEmailCorrect() {
+    var emailPattern = /[a-z0-9]{2,}@[a-z0-9-]{2,}\.[a-z0-9]{2,}/;
+
+    if(email.value === ""){
+      error[0].innerHTML = "필수 정보입니다.";
+      error[0].style.display = "block";
+    } else if(!emailPattern.test(email.value)) {
+      error[0].innerHTML = "이메일형식으로 다시 입력해주세요.";
+      error[0].style.display = "block";
     } else {
-        error[0].innerHTML = "멋진 아이디네요!";
-        error[0].style.color = "#08A600";
-        error[0].style.display = "block";
+      error[0].innerHTML = "멋진 아이디네요!";
+      error[0].style.color = "#08A600";
+      error[0].style.display = "block";
     }
+
 }
 
 function checkPw() {
@@ -79,22 +77,22 @@ function checkPw() {
         error[1].style.display = "block";
 
         pwMsg.style.display = "block";
-        pwImg1.src = "m_icon_not_use.png";
+        pwImg1.src = "/Users/iseungseob/Desktop/Database-Hotel-WebSite/Front Example/img/m_icon_not_use.png";
     } else {
         error[1].style.display = "none";
         pwMsg.innerHTML = "안전";
         pwMsg.style.display = "block";
         pwMsg.style.color = "#03c75a";
-        pwImg1.src = "m_icon_safe.png";
+        pwImg1.src = "/Users/iseungseob/Desktop/Database-Hotel-WebSite/Front Example/img/m_icon_safe.png";
     }
 }
 
 function comparePw() {
     if(pw2.value === pw1.value && pw2.value != "") {
-        pwImg2.src = "m_icon_check_enable.png";
+        pwImg2.src = "/Users/iseungseob/Desktop/Database-Hotel-WebSite/Front Example/img/m_icon_check_enable.png";
         error[2].style.display = "none";
     } else if(pw2.value !== pw1.value) {
-        pwImg2.src = "m_icon_check_disable.png";
+        pwImg2.src = "/Users/iseungseob/Desktop/Database-Hotel-WebSite/Front Example/img/m_icon_check_disable.png";
         error[2].innerHTML = "비밀번호가 일치하지 않습니다.";
         error[2].style.display = "block";
     }
@@ -171,20 +169,6 @@ function checkAge() {
     } else {
         error[4].style.display = "none";
     }
-}
-
-
-function isEmailCorrect() {
-    var emailPattern = /[a-z0-9]{2,}@[a-z0-9-]{2,}\.[a-z0-9]{2,}/;
-
-    if(email.value === ""){
-        error[6].style.display = "none";
-    } else if(!emailPattern.test(email.value)) {
-        error[6].style.display = "block";
-    } else {
-        error[6].style.display = "none";
-    }
-
 }
 
 function checkPhoneNum() {
