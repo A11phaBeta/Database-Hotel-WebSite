3ecad8f959e14d564f479b816f884b2b32d9175e
A11phaBeta <alp69799@hanyang.ac.kr>
2020-11-15 14:08:18 +0900

    Add Database Configuration file and make structure at api controller

--- api/apiControl.php
+++ api/apiControl.php
@@ -1,1 +1,18 @@
-fsfdsfds
+<?php
+
+if(strlen($_SERVER['QUERY_STRING'])){
+  echo json_encode(array('result'=>'fail', 'cause'=>'UNDEFINED_QUERY_STRING'));
+  exit;
+}
+
+switch($_SERVER['QUERY_STRING']){
+  case 'login' :
+    
+    break;
+  case 'signup' :
+    break;
+}
+
+
+
+ ?>
---
+++ api/config/DBConfig.php
@@ -0,0 +1,17 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'alld');
+define('DB_PASS', 'alp03100716');
+define('DB_NAME', 'alld');
+
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if(!$conn){
+  echo json_encode(array('result'=>'fail', 'cause'=>'DATABASE_CONNECTION_ERROR'));
+}
+
+
+
+ ?>
