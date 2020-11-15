b961501cf275f0f8e2e2c83195a8673ba27dab66
A11phaBeta <alp69799@hanyang.ac.kr>
2020-11-15 14:28:08 +0900

    Add Login Function (Finish Testing)

--- api/config/DBConfig.php
+++ api/config/DBConfig.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'alld');
-define('DB_PASS', 'alp03100716');
-define('DB_NAME', 'alld');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Hotel');
 
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
