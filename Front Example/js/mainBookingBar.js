0ca3ef3c8de55246d5ed105b0d57f79a16a45ab2
ab13696802@gmail.com <lcs3623@naver.com>
2020-11-16 20:13:08 +0900

    frontend(booking.html)

--- Front Example/js/mainBookingBar.js
+++ Front Example/js/mainBookingBar.js
@@ -12,7 +12,7 @@ $(function () {
     }
     num = $(".num:eq(" + n + ")").val(num * 1 - 1);
   });
-  
+
   $('.countUp').click(function () {
     var n = $('.countUp').index(this);
     var num = $(".num:eq(" + n + ")").val();
@@ -21,4 +21,50 @@ $(function () {
     }
     num = $(".num:eq(" + n + ")").val(num * 1 + 1);
   });
-}) 
+})
+
+function inputMoveNumber(num) {
+	if(isFinite(num.value) == false) {
+		alert("카드번호는 숫자만 입력할 수 있습니다.");
+		  num.value = "";
+			return false;
+	}
+	max = num.getAttribute("maxlength");
+	if(num.value.length >= max) {
+		num.nextElementSibling.focus();
+	}
+}
+
+function inputValidThru(period) {
+  var replaceCard = period.value.replace(/\//g, "");
+  if(replaceCard.length >= 4 && replaceCard.length < 5) {
+    var inputMonth = replaceCard.substring(0, 2);
+    var inputYear = replaceCard.substring(2, 4);
+    var nowDate = new Date();
+    var nowMonth = autoLeftPad(nowDate.getMonth() + 1, 2);
+    var nowYear = autoLeftPad(nowDate.getFullYear().toString().substr(2, 2), 2);
+    if(isFinite(inputMonth + inputYear) == false) {
+      alert("문자는 입력하실 수 없습니다.");
+      period.value = autoLeftPad((Number(nowMonth) + 1), 2) + "/" + nowYear;
+      return false;
+    }
+    if(inputMonth > 12) {
+      alert("12월보다 큰 월수는 입력하실 수 없습니다. ");
+      period.value = "12/" + inputYear;
+      return false;
+    }
+    if((inputYear + inputMonth) <= (nowYear + nowMonth)) {
+      alert("유효기간이 만료된 카드는 사용하실 수 없습니다.");
+      period.value = inputMonth + "/" + autoLeftPad((Number(nowYear) + 1), 2);
+      return false;
+    }
+    period.value = inputMonth + "/" + inputYear;
+   }
+}
+
+function autoLeftPad(num, digit) {
+  if(String(num).length < digit) {
+    num = new Array(digit - String(num).length + 1).join('0') + num;
+  }
+  return num;
+}
